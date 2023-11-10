fe747f24a04ca2886bcc88ee5ad49eebdfcd9ce1
Jesse Reynolds <jessereynolds18@gmail.com>
2023-11-10 01:16:30 -0600

    start on CSS and styling, along with starting a dark mode/light mode button

--- Project Files/index.js
+++ Project Files/index.js
@@ -1,34 +1,76 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM Content is loaded");
 
-  function populateDropdown() {
+  function createOption(classData) {
+    const option = document.createElement("option");
+    option.value = classData.class;
+    option.text = classData.class;
+    return option;
+  }
+
+  function populateDropdownAndDisplayInfo(classesData) {
     const classDropdown = document.getElementById("classDropdown");
+    const classInfoDiv = document.getElementById("classInfo");
     const defaultOption = document.createElement("option");
     defaultOption.value = "";
     defaultOption.text = "Select a class";
     classDropdown.add(defaultOption);
 
-    // This block of code will fetch the information from the JSON-Server
-    fetch('http://localhost:3000/classes')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Response Error');
-        }
-        return response.json();
-      })
-      .then(classesData => {
-        // Loop through each class in the JSON data and add an option to the dropdown. forEach project goal satisfied
-        classesData.forEach(classObj => {
-          const option = document.createElement("option");
-          option.value = classObj.class;
-          option.text = classObj.class;
-          classDropdown.add(option);
-        });
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+    // Loop through each class in the JSON data and add an option to the dropdown. Satisfies the project requirement.
+    // In this forEach, it adds an option and creates it using the information in the db.json file.
+    classesData.forEach(classData => {
+      const option = createOption(classData);
+      classDropdown.add(option);
+    });
+
+    classDropdown.addEventListener("change", () => {
+      const selectedClass = classDropdown.value;
+
+      if (selectedClass !== "") {
+        // Find the selected class in the fetched data
+        const selectedClassData = classesData.find(classData => classData.class === selectedClass);
+
+        // Display Core Ability and Feature beneath the dropdown
+        updateClassInfo(selectedClassData, classInfoDiv);
+      } else {
+        // Clear the class info if the default option is selected
+        clearClassInfo(classInfoDiv);
+      }
+    });
   }
 
-  populateDropdown();
+  function createParagraph(content) {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = content;
+    return paragraph;
+  }
+
+  function updateClassInfo(classData, classInfoDiv) {
+    classInfoDiv.innerHTML = ""; // Clears previous content. InnerHTML is not secure but this should be fine for the project.
+
+    const coreAbilityParagraph = createParagraph(`Core Ability: ${classData.core_ability}`);
+    const featureParagraph = createParagraph(`Feature: ${classData.feature}`);
+
+    classInfoDiv.appendChild(coreAbilityParagraph);
+    classInfoDiv.appendChild(featureParagraph);
+  }
+
+  function clearClassInfo(classInfoDiv) {
+    classInfoDiv.innerHTML = "";
+  }
+
+  // This block of code will fetch all class information from the JSON-Server
+  fetch('http://localhost:3000/classes')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Response Error');
+      }
+      return response.json();
+    })
+    .then(classesData => {
+      populateDropdownAndDisplayInfo(classesData);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 });
